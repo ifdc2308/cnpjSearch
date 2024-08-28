7d0222be1d538b616cd9e622a44792f84fcbb354
@ifdc2308 <italodorea123@gmail.com>
2024-08-28 13:38:16 -0300

    feat: input-search

--- ClientApp/src/pages/Home.tsx
+++ ClientApp/src/pages/Home.tsx
@@ -1,11 +1,45 @@
 import React from "react";
 import ResponsiveAppBar from "../components/AppBar";
+import { Container, Grid, Paper, IconButton, InputBase } from "@mui/material";
+import { Search, Menu } from "@mui/icons-material";
 
 const DashboardPage: React.FC = () => {
   return (
     <>
       <ResponsiveAppBar />
-      <p>Pesquisa por CNPJ</p>
+      <Container
+        maxWidth="lg"
+        sx={{
+          p: 3,
+          minHeight: "100vh",
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <IconButton sx={{ p: "10px" }} aria-label="menu">
+                <Menu />
+              </IconButton>
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Digitar CNPJ"
+                inputProps={{ "aria-label": "Pesquisar CNPJ" }}
+              />
+              <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+                <Search />
+              </IconButton>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
